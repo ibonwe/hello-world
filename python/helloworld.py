28013487c03a73712863fa367a1b21f01bcb98d0
Melanie Molifie <mmolifie@gmail.com>
2020-05-10 16:15:41 +0200

    Another change

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,3 +1,14 @@
 # Short code written in python
+# adding python code
 
 print("My name is Melanie")
+
+for i in range(2,101):
+    x=1
+    for j in range(2, i):
+        n=i%j
+        if n==0:
+            x=0
+            break
+    if x==1:
+        print (i)
