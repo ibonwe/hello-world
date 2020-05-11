4d9f36a4c6c4d1e68c79e79defb17a1b9f37bf5b
Melanie Molifie <mmolifie@gmail.com>
2020-05-11 21:00:42 +0200

    Added a prime list function to existing code

--- python/helloworld.py
+++ python/helloworld.py
@@ -1,14 +1,18 @@
 # Short code written in python
 # Code added to print a prime number on each line
 
-print("My name is Melanie")
+a= int(input("Insert any number more than 2: "))
 
-for i in range(2,101):
-    x=1
-    for j in range(2, i):
-        n=i%j
-        if n==0:
-            x=0
-            break
-    if x==1:
-        print (i)
+def primeList(a):
+    for i in range(2, a):
+        x=1
+        for j in range(2, i):
+            n=i%j
+            if n==0:
+                x=0
+                break
+        if x==1:
+            print(i)
+
+print("My name is Melanie and my prime list from 2 till %d is: " %(a))       
+primeList(a)
