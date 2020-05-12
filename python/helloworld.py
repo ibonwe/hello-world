d57b1800a35219e7885061d479318dd35230d1fe
Melanie Molifie <mmolifie@gmail.com>
2020-05-12 20:28:44 +0200

    Changes made

--- python/helloworld.py
+++ python/helloworld.py
@@ -3,16 +3,4 @@
 
 a= int(input("Insert any number more than 2: "))
 
-def primeList(a):
-    for i in range(2, a):
-        x=1
-        for j in range(2, i):
-            n=i%j
-            if n==0:
-                x=0
-                break
-        if x==1:
-            print(i)
-
-print("My name is Melanie and my prime list from 2 till %d is: " %(a))       
-primeList(a)
+print("My name is Melanie and my prime list from 2 till %d is: " %(a))
